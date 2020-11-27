003df4e5fc8b98de93b07b2b2136b91b0ba97832
11ume <horacioriverods@gmail.com>
2020-11-26 23:20:09 -0300

    fix in types

--- packages/oauth2-client/src/client.ts
+++ packages/oauth2-client/src/client.ts
@@ -1,47 +1,43 @@
 import querystring from 'querystring'
-import { RequestInit } from 'node-fetch'
+// import fetch from 'node-fetch'
 
-type Request = (url: string, options?: RequestInit) => Promise<Response>
-type GenerateAuthUrlOptions = {
-    [key: string]: any
-}
 interface OAuth2ClientOptions {
     readonly clientId: string
     readonly clientSecret: string
     readonly redirectUri: string
     readonly authenticationUrl: string
-    readonly request: Request
+}
+
+export type GenerateAuthUrlOptions = {
+    [key: string]: any
 }
 export class OAuth2Client {
     readonly clientId: string
     readonly clientSecret: string
     readonly redirectUri: string
     readonly authenticationUrl: string
-    readonly request: Request
     constructor({
         clientId
         , clientSecret
         , redirectUri
         , authenticationUrl
-        , request
     }: OAuth2ClientOptions) {
         this.clientId = clientId
         this.clientSecret = clientSecret
         this.redirectUri = redirectUri
         this.authenticationUrl = authenticationUrl
-        this.request = request
     }
 
     generateAuthUrl(options: GenerateAuthUrlOptions): string {
-        let scope: string | string[]
+        let scope = options.scope
         if (options.scope instanceof Array) {
             scope = options.scope.join(' ')
         }
-        const mergeOpts = {
+        const params = querystring.stringify({
             ...options
             , scope
-        }
-        return this.authenticationUrl + '?' + querystring.stringify(mergeOpts)
+        })
+        return `${this.authenticationUrl}?${params}`
     }
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
--- packages/oauth2/src/authProvider.ts
+++ packages/oauth2/src/authProvider.ts
@@ -2,46 +2,38 @@ import querystring from 'querystring'
 import { IStorage } from 'fure-storage'
 import { getRequiredParam, isStore } from 'fure-shared'
 import { IUniqueSessionTokenManager } from 'fure-ustm'
-import { OAuth2Client } from 'fure-oauth2-client'
-
-export interface IOAuth2Client {
-    readonly clientId: string
-    readonly clientSecret: string
-    readonly redirectUri: string
-    generateAuthUrl(): string
-    callbackHandler(): any
-    revokeToken(): any
-}
+import { OAuth2Client, GenerateAuthUrlOptions } from 'fure-oauth2-client'
 
 export interface IFureOAuth2Provider {
-    readonly clientId: string
-    readonly clientSecret: string
-    readonly redirectUri: string
-    generateAuthUrl(): string
+    generateAuthUrl(options: GenerateAuthUrlOptions): string
     callbackHandler(): any
     revokeToken(): any
 }
 
 export interface OAuth2ProviderOptions {
     readonly provider: string
-    readonly clientId: string
-    readonly clientSecret: string
-    readonly authenticationUrl: string
     readonly authPath: string
-    readonly redirectUri: string
     readonly state?: boolean
     readonly scope?: string[]
     readonly store?: IStorage
     readonly oAuth2Client: OAuth2Client
-    readonly uniqueSessionTokenManager: IUniqueSessionTokenManager
+    readonly uniqueSessionTokenManager?: IUniqueSessionTokenManager
 }
 
 export class FureOAuth2Provider {
     readonly provider: string
-    readonly clientId: string
-    readonly clientSecret: string
+    /**
+     * The client ID for your application. The value passed into the constructor
+     * will be used if not provided. You can find this value in the API Console.
+     */
     readonly authPath: string
-    readonly redirectUri: string
+    /**
+     * Determines where the API server redirects the user after the user
+     * completes the authorization flow. The value must exactly match one of the
+     * 'redirect_uri' values listed for your project in the API Console. Note that
+     * the http or https scheme, case, and trailing slash ('/') must all match.
+     * The value passed into the constructor will be used if not provided.
+     */
     readonly state: boolean
     readonly scope: string[]
     readonly authenticationUrl: string
@@ -51,29 +43,21 @@ export class FureOAuth2Provider {
     protected readonly uniqueSessionTokenManager: IUniqueSessionTokenManager
     protected constructor({
         provider
-        , clientId
-        , clientSecret
-        , authenticationUrl
         , authPath
-        , redirectUri
         , state
         , scope
         , store
         , oAuth2Client
-        , uniqueSessionTokenManager
+        , uniqueSessionTokenManager = null
     }: OAuth2ProviderOptions) {
         this.provider = provider
-        this.clientId = clientId
-        this.clientSecret = clientSecret
-        this.authenticationUrl = authenticationUrl
         this.authPath = authPath
-        this.redirectUri = redirectUri
         this.state = state
         this.scope = scope
         this.store = store
         this.oAuth2Client = oAuth2Client
         this.uniqueSessionTokenManager = uniqueSessionTokenManager
-        this.parsedRedirectUrl = new URL(this.redirectUri)
+        this.parsedRedirectUrl = new URL(this.oAuth2Client.redirectUri)
         this.state && this.checkStorage()
     }
 
