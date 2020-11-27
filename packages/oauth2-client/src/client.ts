15d21fc6013d253849ac61a25bb5607fcd3f2229
11ume <horacioriverods@gmail.com>
2020-11-26 21:20:11 -0300

    add code channalge

--- packages/oauth2-client/src/client.ts
+++ packages/oauth2-client/src/client.ts
@@ -2,6 +2,7 @@ import { RequestInit } from 'node-fetch'
 
 export type Request = (url: string, options?: RequestInit) => Promise<Response>
 
+type CodeChallengeMethod = 'plain' | 'S256'
 export interface IOAuth2Client {
     readonly clientId: string
     readonly clientSecret: string
@@ -22,7 +23,24 @@ export class OAuth2Client implements IOAuth2Client {
     readonly clientId: string
     readonly clientSecret: string
     readonly redirectUri: string
-    readonly #request: Request
+    readonly request: Request
+
+    /**
+     * Recommended. Specifies what method was used to encode a 'code_verifier'
+     * that will be used during authorization code exchange. This parameter must
+     * be used with the 'code_challenge' parameter. The value of the
+     * 'code_challenge_method' defaults to "plain" if not present in the request
+     * that includes a 'code_challenge'. The only supported values for this
+     * parameter are "S256" or "plain".
+     */
+    readonly codeChallengeMethod?: CodeChallengeMethod
+    /**
+     * Recommended. Specifies an encoded 'code_verifier' that will be used as a
+     * server-side challenge during authorization code exchange. This parameter
+     * must be used with the 'code_challenge' parameter described above.
+     */
+    readonly codeChallenge?: string
+
     constructor({
         clientId
         , clientSecret
@@ -32,7 +50,7 @@ export class OAuth2Client implements IOAuth2Client {
         this.clientId = clientId
         this.clientSecret = clientSecret
         this.redirectUri = redirectUri
-        this.#request = request
+        this.request = request
     }
 
     generateAuthUrl(): string {
